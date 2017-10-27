1424a09126e4e3d36c2a4e8c204f4bb3930b9a6c
alefarias-dev <mail.alefarias@gmail.com>
2017-10-27 07:48:24 -0200

    solucao1: rw; solucao2: busy wait

--- src/src/solucao1/solucao1.java
+++ src/src/solucao1/solucao1.java
@@ -10,27 +10,66 @@ package solucao1;
  * the database they will be using to coordinate access.
  */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
 import java.util.concurrent.Semaphore;
 
-class ReaderWriterSolution {
-	public static final int NUM_OF_READERS = 60;
-	public static final int NUM_OF_WRITERS = 40;
 
-	public static void main(String args[]) {
-		RWLock database = new Database();
-
-		Thread[] readerArray = new Thread[NUM_OF_READERS];
-		Thread[] writerArray = new Thread[NUM_OF_WRITERS];
-
-		for (int i = 0; i < NUM_OF_READERS; i++) {
-			readerArray[i] = new Thread(new Reader(i, database));
-			readerArray[i].start();
+class SimuladorRW {
+	
+	public static int NUM_OF_READERS = 3;
+	public static int NUM_OF_WRITERS = 2;
+	
+	// configura quantidade de readers e writers
+	public static void setRW(int r, int w) { NUM_OF_READERS = r; NUM_OF_READERS = w; }
+	
+	public SimuladorRW(int readers, int writers) { setRW(readers, writers); }
+	
+	public static String[] carregarDatabase () throws Exception {
+		
+		// abre arquivo bd.txt
+		String nomeArquivo = "bd/bd.txt";
+		FileReader        fr    = new FileReader(nomeArquivo);
+		BufferedReader    br    = new BufferedReader(fr);
+		ArrayList<String> lines = new ArrayList<String>();
+		
+		// coloca linha a linha na string line
+		String line = null;
+		while ((line = br.readLine()) != null) lines.add(line);
+		
+		// fecha arquivo bd.txt
+		br.close();
+		
+		// retorna o array com todas as linhas do arquivo txt
+		return lines.toArray(new String[lines.size()]);
+	}
+	
+	public static void main(String args[]) throws Exception {
+		
+		// cria o database
+		String[] db = carregarDatabase();
+		RWLock database = new Database(db);
+		System.out.println("rodando solucao readers and writers");
+		
+		// configura qtde de readers e writers
+		setRW(2, 5);
+		
+		// arranjo com todos os objetos escritores e leitores
+		// e cria objetos readers e writers
+		Thread[] readers_writers = new Thread[NUM_OF_READERS+NUM_OF_WRITERS];
+		for (int i = 0; i < NUM_OF_READERS + NUM_OF_WRITERS; i++) {
+			if (i < NUM_OF_READERS) readers_writers[i] = new Thread(new Reader(i, database));
+			else readers_writers[i] = new Thread(new Writer(i, database));
 		}
-
-		for (int i = 0; i < NUM_OF_WRITERS; i++) {
-			writerArray[i] = new Thread(new Writer(i, database));
-			writerArray[i].start();
-		}
+		
+		// embaralha todos os objetos
+		List<Thread> rw = Arrays.asList(readers_writers);
+		Collections.shuffle(rw);
+		readers_writers = (Thread[]) rw.toArray();
+			
+		// apos embaralhar
+		for (int i = 0; i < NUM_OF_WRITERS + NUM_OF_WRITERS; i++) readers_writers[i].start();		
 	}
 }
 
@@ -66,11 +105,13 @@ class Database implements RWLock {
 	private int readerCount; // the number of active readers
 	private Semaphore mutex; // controls access to readerCount
 	private Semaphore db; // controls access to the database
+	public String[] database; // possui as linhas da base de dados 
 
-	public Database() {
+	public Database(String[] database) {
 		readerCount = 0;
 		mutex = new Semaphore(1);
 		db = new Semaphore(1);
+		this.database = database;
 	}
 
 	public void acquireReadLock(int readerNum) {
@@ -154,12 +195,14 @@ class Reader implements Runnable {
 	}
 
 	public void run() {
-		while (true) {
+		int i = 0;
+		while (i < 5) {
 			SleepUtilities.nap();
 
 			System.out.println("reader " + readerNum + " wants to read.");
 			database.acquireReadLock(readerNum);
-
+			
+			i++;
 			// you have access to read from the database
 			// let's read for awhile .....
 			SleepUtilities.nap();
@@ -187,12 +230,15 @@ class Writer implements Runnable {
 	}
 
 	public void run() {
-		while (true) {
+		int i = 0;
+		while (i < 5) {
 			SleepUtilities.nap();
 
 			System.out.println("writer " + writerNum + " wants to write.");
 			database.acquireWriteLock(writerNum);
-
+			
+			i++;
+			
 			// you have access to write to the database
 			// write for awhile ...
 			SleepUtilities.nap();
--- src/src/solucao2/solucao2.java
+++ src/src/solucao2/solucao2.java
@@ -1,169 +1,237 @@
 package solucao2;
 /**
- * This class represents a database. There are many competing threads wishing to
- * read and write. It is acceptable to have multiple processes reading at the
- * same time, but if one thread is writing then no other process may either read
- * or write.
+ * Original code by Silberschatz, Galvin, and Gagne
+ * from Operating System Concepts with Java, 7th Edition
+ * Modified by William McDaniel Albritton
+ *
+ * ReaderWriterSolution.java
+ *
+ * This class creates the reader and writer threads and
+ * the database they will be using to coordinate access.
  */
-class Database {
-	private int readers; // number of active readers
 
-	/**
-	 * Initializes this database.
-	 */
-	public Database() {
-		this.readers = 0;
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.Semaphore;
+
+class Simulador {
+	
+	public static int NUM_OF_READERS = 3;
+	public static int NUM_OF_WRITERS = 2;
+	
+	// configura quantidade de readers e writers
+	public static void setRW(int r, int w) { NUM_OF_READERS = r; NUM_OF_READERS = w; }
+	
+	public Simulador(int readers, int writers) { setRW(readers, writers); }
+	
+	public static String[] carregarDatabase () throws Exception {
+		
+		// abre arquivo bd.txt
+		String nomeArquivo = "bd/bd.txt";
+		FileReader        fr    = new FileReader(nomeArquivo);
+		BufferedReader    br    = new BufferedReader(fr);
+		ArrayList<String> lines = new ArrayList<String>();
+		
+		// coloca linha a linha na string line
+		String line = null;
+		while ((line = br.readLine()) != null) lines.add(line);
+		
+		// fecha arquivo bd.txt
+		br.close();
+		
+		// retorna o array com todas as linhas do arquivo txt
+		return lines.toArray(new String[lines.size()]);
 	}
-
-	/**
-	 * Read from this database.
-	 * 
-	 * @param number
-	 *            Number of the reader.
-	 */
-	public void read(int number) {
-		synchronized (this) {
-			this.readers++;
-			System.out.println("Reader " + number + " starts reading.");
+	
+	public static void main(String args[]) throws Exception {
+		
+		// cria o database
+		String[] db = carregarDatabase();
+		RWLock database = new Database(db);
+		System.out.println("rodando solucao busy");
+		
+		// configura qtde de readers e writers
+		setRW(2, 5);
+		
+		// arranjo com todos os objetos escritores e leitores
+		// e cria objetos readers e writers
+		Thread[] readers_writers = new Thread[NUM_OF_READERS+NUM_OF_WRITERS];
+		for (int i = 0; i < NUM_OF_READERS + NUM_OF_WRITERS; i++) {
+			if (i < NUM_OF_READERS) readers_writers[i] = new Thread(new Reader(i, database));
+			else readers_writers[i] = new Thread(new Writer(i, database));
 		}
+		
+		// embaralha todos os objetos
+		List<Thread> rw = Arrays.asList(readers_writers);
+		Collections.shuffle(rw);
+		readers_writers = (Thread[]) rw.toArray();
+			
+		// apos embaralhar
+		for (int i = 0; i < NUM_OF_WRITERS + NUM_OF_WRITERS; i++) readers_writers[i].start();		
+	}
+}
 
-		final int DELAY = 5000;
+// ****************************************************************
+
+/**
+ * An interface for reader-writer locks.
+ * 
+ * In the text we do not have readers and writers pass their number into each
+ * method. However we do so here to aid in output messages.
+ */
+
+interface RWLock {
+
+	public abstract void busyLock(Runnable writerNum);
+	
+	public abstract void busyRelease(Runnable writerNum);
+}
+
+// ****************************************************************
+
+/**
+ * Database.java
+ * 
+ * This class contains the methods the readers and writers will use to
+ * coordinate access to the database. Access is coordinated using semaphores.
+ */
+
+class Database implements RWLock {
+	private int readerCount; // the number of active readers
+	private Semaphore mutex; // controls access to readerCount
+	private Semaphore db; // controls access to the database
+	public String[] database; // possui as linhas da base de dados 
+
+	public Database(String[] database) {
+		readerCount = 0;
+		mutex = new Semaphore(1);
+		db = new Semaphore(1);
+		this.database = database;
+	}
+	
+	public void busyLock(Runnable rw) {
 		try {
-			Thread.sleep((int) (Math.random() * DELAY));
+			db.acquire();
 		} catch (InterruptedException e) {
 		}
+		
+		if (rw instanceof Writer) System.out.println("Writer " + ((Writer) rw).getId() + " is writing.");
+		if (rw instanceof Reader) System.out.println("Reader " + ((Reader) rw).getId() + " is reading.");
+	}
+	
+	public void busyRelease(Runnable rw) {
+	
+		if (rw instanceof Writer) System.out.println("Writer " + ((Writer) rw).getId() + " is done writing.");
+		if (rw instanceof Reader) System.out.println("Reader " + ((Reader) rw).getId() + " is done reading.");
+		db.release();
+	}
 
-		synchronized (this) {
-			System.out.println("Reader " + number + " stops reading.");
-			this.readers--;
-			if (this.readers == 0) {
-				this.notifyAll();
-			}
+}
+
+// ***********************************************************
+
+/**
+ * Reader.java
+ * 
+ * A reader to the database.
+ * 
+ */
+
+class Reader implements Runnable {
+
+	private RWLock database;
+	private int readerNum;
+
+	public Reader(int readerNum, RWLock database) {
+		this.readerNum = readerNum;
+		this.database = database;
+	}
+	
+	public int getId() { return this.readerNum; }
+
+	public void run() {
+		int i = 0;
+		while (i < 5) {
+			SleepUtilities.nap();
+
+			System.out.println("reader " + readerNum + " wants to read.");
+			database.busyLock(this);
+			
+			i++;
+			// you have access to read from the database
+			// let's read for awhile .....
+			SleepUtilities.nap();
+
+			database.busyRelease(this);
+		}
+	};
+}
+
+// **************************************************************
+
+/**
+ * Writer.java
+ * 
+ * A writer to the database.
+ * 
+ */
+class Writer implements Runnable {
+	private RWLock database;
+	private int writerNum;
+
+	public Writer(int w, RWLock d) {
+		writerNum = w;
+		database = d;
+	}
+	
+	public int getId() { return this.writerNum; }
+
+	public void run() {
+		int i = 0;
+		while (i < 5) {
+			SleepUtilities.nap();
+
+			System.out.println("writer " + writerNum + " wants to write.");
+			database.busyLock(this);
+			
+			i++;
+			
+			// you have access to write to the database
+			// write for awhile ...
+			SleepUtilities.nap();
+
+			database.busyRelease(this);
 		}
 	}
 
-	/**
-	 * Writes to this database.
-	 * 
-	 * @param number
-	 *            Number of the writer.
-	 */
-	public synchronized void write(int number) {
-		while (this.readers != 0) {
-			try {
-				this.wait();
-			} catch (InterruptedException e) {
-			}
-		}
-		System.out.println("Writer " + number + " starts writing.");
+}
 
-		final int DELAY = 5000;
+// *****************************************************************
+
+/**
+ * Utilities for causing a thread to sleep. Note, we should be handling
+ * interrupted exceptions but choose not to do so for code clarity.
+ * 
+ */
+
+class SleepUtilities {
+	/**
+	 * Nap between zero and NAP_TIME seconds.
+	 */
+	public static void nap() {
+		nap(NAP_TIME);
+	}
+
+	/**
+	 * Nap between zero and duration seconds.
+	 */
+	public static void nap(int duration) {
+		int sleeptime = (int) (NAP_TIME * Math.random());
 		try {
-			Thread.sleep((int) (Math.random() * DELAY));
+			Thread.sleep(sleeptime * 1000);
 		} catch (InterruptedException e) {
 		}
-
-		System.out.println("Writer " + number + " stops writing.");
-		this.notifyAll();
-	}
-}
-
-/**
- * This class represents a reader.
- */
-class Reader extends Thread {
-	private static int readers = 0; // number of readers
-
-	private int number;
-	private Database database;
-
-	/**
-	 * Creates a Reader for the specified database.
-	 * 
-	 * @param database
-	 *            database from which to be read.
-	 */
-	public Reader(Database database) {
-		this.database = database;
-		this.number = Reader.readers++;
 	}
 
-	/**
-	 * Reads.
-	 */
-	public void run() {
-		while (true) {
-			final int DELAY = 5000;
-			try {
-				Thread.sleep((int) (Math.random() * DELAY));
-			} catch (InterruptedException e) {
-			}
-			this.database.read(this.number);
-		}
-	}
-}
-
-/**
- * This class represents a writer.
- */
-class Writer extends Thread {
-	private static int writers = 0; // number of writers
-
-	private int number;
-	private Database database;
-
-	/**
-	 * Creates a Writer for the specified database.
-	 * 
-	 * @param database
-	 *            database to which to write.
-	 */
-	public Writer(Database database) {
-		this.database = database;
-		this.number = Writer.writers++;
-	}
-
-	/**
-	 * Writes.
-	 */
-	public void run() {
-		while (true) {
-			final int DELAY = 5000;
-			try {
-				Thread.sleep((int) (Math.random() * DELAY));
-			} catch (InterruptedException e) {
-			}
-			this.database.write(this.number);
-		}
-	}
-}
-
-/**
- * This app creates a specified number of readers and writers and starts them.
- */
-class Simulator {
-	/**
-	 * Creates the specified number of readers and writers and starts them.
-	 * 
-	 * @param args
-	 *            [0] The number of readers.
-	 * @param args
-	 *            [1] The number of writers.
-	 */
-	public static void main(String[] args) {
-		if (args.length < 2) {
-			System.out
-					.println("Usage: java Simulator <number of readers> <number of writers>");
-		} else {
-			final int READERS = Integer.parseInt(args[0]);
-			final int WRITERS = Integer.parseInt(args[1]);
-			Database database = new Database();
-			for (int i = 0; i < READERS; i++) {
-				new Reader(database).start();
-			}
-			for (int i = 0; i < WRITERS; i++) {
-				new Writer(database).start();
-			}
-		}
-	}
+	private static final int NAP_TIME = 5;
 }
